a2f5c3f0114eab52d53092e7370c6def869b4629
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-05 16:31:28 +0100

    Encrypting passwords with Bcrypt

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,7 +1,9 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
-const { User, validate } = require('../models/userModel');
 const mongoose = require('mongoose');
+const { User, validate } = require('../models/userModel');
+
 
 const router = express();
 
@@ -15,6 +17,8 @@ router.post('/', async (req, res) => {
   if (user) return res.status(400).send('User already registered.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   res.send(_.pick(user, ['name', 'email']));
