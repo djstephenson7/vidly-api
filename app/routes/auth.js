43658223f0dd21cb10024b043cb7538b22bae547
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-14 12:54:05 +0100

    Hardcoded JSON Web Token

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const mongoose = require('mongoose');
 const Joi = require('joi');
@@ -18,7 +19,8 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+  res.send(token);
 });
 
 function validate(req) {
