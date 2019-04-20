8f7c35688969a88e090ea705bf4b74155f2e3264
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-20 20:41:57 +0100

    Extracted config logic

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('config');
 
 const app = express();
 
 require('./app/startup/logging')();
 require('./app/startup/routes')(app);
 require('./app/startup/db')(mongoose);
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL: jwtPrivateKey is not defined');
-  process.exit(1);
-}
+require('./app/startup/config')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
