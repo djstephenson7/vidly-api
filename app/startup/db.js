1d3716276671a18caf2428c67961a19861d8f617
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-20 20:26:29 +0100

    Extracted error logging logic

--- app/startup/db.js
+++ app/startup/db.js
@@ -1,6 +1,6 @@
 const winston = require('winston');
 
-module.exports = function(mongoose) {
+module.exports = function (mongoose) {
   mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
     .then(() => console.log('Connecting to MongoDB...'))
     .catch(() => console.log('Could not connect to MongoDB!'));
--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-const winston = require('winston');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
-require('winston-mongodb');
-require('express-async-errors');
 
 const app = express();
 
+require('./app/startup/logging')();
 require('./app/startup/routes')(app);
 require('./app/startup/db')(mongoose);
-
-winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
-
-process.on('unhandledRejection', (ex) => {
-  throw ex;
-});
-
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, {
-  db: 'mongodb://localhost/vidly',
-  level: 'info',
-});
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL: jwtPrivateKey is not defined');
