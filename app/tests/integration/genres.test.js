d7754d71900f98c02d7cd1dd58cf530f6f90e487
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-23 17:56:18 +0100

    Code coverage & genre 404 int test

--- app/tests/integration/genres.test.js
+++ app/tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const mongoose = require('mongoose');
 const { Genre } = require('../../models/genreModel');
 const { User } = require('../../models/userModel');
 
@@ -39,6 +40,13 @@ describe('GET/:id', () => {
 
   it('Should return 404 if invalid id is passed', async () => {
     const res = await request(server).get('/api/genres/1');
+
+    expect(res.status).toBe(404);
+  });
+
+  it('Should return 404 if no genre with the given id exists', async () => {
+    const id = mongoose.Types.ObjectId();
+    const res = await request(server).get(`/api/genres/${id}`);
 
     expect(res.status).toBe(404);
   });
