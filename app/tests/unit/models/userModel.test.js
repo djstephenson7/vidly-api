fef5025fd2e37c823de69b8a11457e0d2a2ae359
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-23 11:28:22 +0100

    Unit test generateAuthToken

---
+++ app/tests/unit/models/userModel.test.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const mongoose = require('mongoose');
+const { User } = require('../../../models/userModel');
+
+describe('user.generateAuthToken', () => {
+  it('Should return a valid JSON Web Token', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+
+    expect(decoded).toMatchObject(payload);
+  });
+});
