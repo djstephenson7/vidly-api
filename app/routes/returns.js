7fcbeb64a7df0741aaaaf311a06fbaec8f1cd603
djstephenson7 <dan_0502@hotmail.co.uk>
2019-05-09 12:18:54 +0100

    Rental 404 if not found

--- app/routes/returns.js
+++ app/routes/returns.js
@@ -9,6 +9,9 @@ router.use(express.json());
 router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  const rental = await Rental.findById(req.params.id);
+  if (!rental) return res.status(404).send('No rental found.');
 });
 
 module.exports = router;
