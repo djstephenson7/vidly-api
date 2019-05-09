8dbdffb8d6cfb1af068b126761c644ad2e0e45b6
djstephenson7 <dan_0502@hotmail.co.uk>
2019-05-09 18:30:27 +0100

    Refactored rental lookup with static methods

--- app/routes/returns.js
+++ app/routes/returns.js
@@ -11,10 +11,7 @@ const router = express.Router();
 router.use(express.json());
 
 router.post('/', [auth, validate(validateReturn)], async (req, res) => {
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) return res.status(404).send('No rental found.');
   if (rental.dateReturned) return res.status(400).send('Rental already processed.');
