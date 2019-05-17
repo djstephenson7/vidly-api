482931d952727853eeedcf920095760e01e8726a
djstephenson7 <dan_0502@hotmail.co.uk>
2019-05-17 13:43:17 +0100

    Fixed Movies DELETE tests

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -43,11 +43,14 @@ router.put('/:id', [auth, validateObjectId], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send('Invalid genre.');
+
   const movie = await Movie.findByIdAndUpdate(req.params.id, {
     title: req.body.title,
     genre: {
       _id: genre._id,
-      name: genre.name,
+      genre: genre.genre,
     },
     numberInStock: req.body.numberInStock,
     dailyRentalRate: req.body.dailyRentalRate,
